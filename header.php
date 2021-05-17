9a103af7429b12f8d5bee4dc603fd1ae541ee79b
Magnus Westhofen <m.westhofen@12-05.de>
2021-05-17 23:51:31 +0200

    fonts

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
         <?php wp_head();?>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Solingen Paladins - <?php the_title();?></title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">   
     </head>
     <body>
         <header class="header">
