a06b947ac4e41d7ab3f1eb140b55d86406b43a5a
Magnus Westhofen <m.westhofen@12-05.de>
2021-06-19 01:17:37 +0200

    test

---
+++ components/live.php
@@ -0,0 +1,16 @@
+<div class="component-live ">
+<div class="wrapped">
+
+    <div class="iframe">
+        <h2><?php echo $block['headline'];?></h2>
+        <?php echo $block['link'];?>
+    </div>
+    <div class="qr">
+      
+        <div>
+            <?php echo $block['qr_text'];?>
+        </div>
+        <img src="<?php echo $block['qr_code'];?>" alt="QR" />
+    </div>
+</div>
+</div>
