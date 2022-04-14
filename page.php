55cdbb5f58a0f3a5c9ef51ca8fdf28d0cf91097b
Magnus Westhofen <m.westhofen@12-05.de>
2022-04-14 21:17:06 +0200

    test

--- page.php
+++ page.php
@@ -1,4 +1,14 @@
 <?php
     get_header();
+    // STARTS - wrapp your content with this conditional statement
+if ( post_password_required() ) :
+
+    // if your post is password protected with our Pro version, show our password form instead
+    echo get_the_password_form();
+
+/* display the password protected content if the correct password is entered */ 
+else :
     get_template_part('pagebuilder');
+endif;
+
     get_footer();
