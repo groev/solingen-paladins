13583ce0cf96c913a33a5a3c2e351e4dd9a1af99
Magnus Westhofen <m.westhofen@12-05.de>
2022-09-30 22:28:40 +0200

    test

--- assets/scripts/custom/custom.js
+++ assets/scripts/custom/custom.js
@@ -41,3 +41,22 @@ jQuery(document).ready(function ($) {
 		}
 	});
 });
+
+window.onscroll = function () {
+	myFunction();
+};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+}
