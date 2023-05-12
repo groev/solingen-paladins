5c8754a0ad6766f1be543a070aec0a8812d32ace
Magnus Westhofen <m.westhofen@12-05.de>
2023-05-13 01:44:55 +0200

    test

--- next/api.php
+++ next/api.php
@@ -63,8 +63,36 @@
             return array(
                 'logo' => get_field('theme_logo', 'option'),
                 'social' => get_field('social_media', 'option'),
-                'menu' => wp_get_nav_menu_items('hauptnavigation')
+                'menu' => 
             );
+        }
+
+        public function get_menu() {
+            $child_items = [];
+            $navbar_items = wp_get_nav_menu_items('hauptnavigation');
+            // pull all child menu items into separate object
+            foreach ($navbar_items as $key => $item) {
+                if ($item->menu_item_parent) {
+                    array_push($child_items, $item);
+                    unset($navbar_items[$key]);
+                }
+            }
+    
+            // push child items into their parent item in the original object
+            foreach ($navbar_items as $item) {
+                foreach ($child_items as $key => $child) {
+                    if ($child->menu_item_parent == $item->post_name) {
+                        if (!$item->child_items) {
+                            $item->child_items = [];
+                        }
+    
+                        array_push($item->child_items, $child);
+                        unset($child_items[$key]);
+                    }
+                }
+            }
+    
+            return $navbar_items;
         }
     }
     $init = new PaladinsNextApi();
